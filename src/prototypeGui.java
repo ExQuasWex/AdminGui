cec244757f2fab28f9c63b6e85fda98a32374aa9
ExQuasWex <villerdex@Gmail.com>
2015-10-09 20:05:30 +0800

    applied slidepane

--- src/prototypeGui.java
+++ src/prototypeGui.java
@@ -4,6 +4,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
 
@@ -15,52 +16,18 @@ public class prototypeGui  extends Stage{
     private static prototypeGui adminWindow = new prototypeGui();
 
     private prototypeGui(){
+        // css settings
 
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 
         BorderPane root = new BorderPane();
 
-        // home tab layout
-        BorderPane hometabLayout = new BorderPane();
+        SlidePane sp = new SlidePane(400);
+        root.setLeft(sp);
 
-        TabPane homeTabpane = new TabPane();
-
-        Tab overAllreportTab = new Tab("OverALl report");
-        Tab suggestionTab = new Tab("Task manager");
-        homeTabpane.getTabs().addAll(overAllreportTab, suggestionTab);
-
-        hometabLayout.setCenter(homeTabpane);
-
-
-        // main tabpane
-        TabPane mainTabpane = new TabPane();
-        mainTabpane.setTabMinWidth(130);
-        mainTabpane.setSide(Side.LEFT);
-
-        //settings of tabs
-        Tab homeTab = new Tab("Home");
-        Tab workTab = new Tab("Work");
-        Tab reportTab = new Tab("Reports");
-        Tab managementTab = new Tab("Management");
-        Tab helpTab = new Tab("Help");
-
-        homeTab.setClosable(false);
-        workTab.setClosable(false);
-        reportTab.setClosable(false);
-        managementTab.setClosable(false);
-        helpTab.setClosable(false);
-
-        homeTab.setContent(hometabLayout);
-
-
-
-        mainTabpane.getTabs().addAll(homeTab,workTab,reportTab,managementTab,helpTab);
-
-        root.setCenter(mainTabpane);
-
-        Scene scene = new Scene(root,screen.getWidth(), screen.getHeight());
+        Scene scene = new Scene(root,800,600);
         setScene(scene);
-
+        initStyle(StageStyle.TRANSPARENT);
         centerOnScreen();
 
     }
